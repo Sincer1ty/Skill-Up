0ef1810a3590a49a2f6355a48117a66508813889
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-10-07 18:14:29 +0900

    Add User API

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,21 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'; // CSS 파일을 추가
+import Search from './components/Search';
 import PostForm from './components/PostForm';
 
 function App() {
+  const [isSearching, setIsSearching] = useState(false);
+
+  const searching = () => {
+    setIsSearching(true);
+  }
+
   return (
     <div className="App">
       <h1>게시글 작성 페이지</h1>
+      <Search isSearching={searching} />
+      {/* {!isSearching && <UserList />} */}
       <PostForm />
     </div>
   );
