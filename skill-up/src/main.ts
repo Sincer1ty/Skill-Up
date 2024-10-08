88295b189aa78d7197e09e4086cda57bdd561679
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-10-08 23:28:18 +0900

    fetch Server

--- skill-up/src/main.ts
+++ skill-up/src/main.ts
@@ -5,7 +5,11 @@ import { User } from './user/schemas/user.schema';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(
+    {origin: 'https://sincer1ty.github.io/Skill-Up/', // 프론트엔드 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true}
+  );
   await app.listen(3000);
 }
 bootstrap();
