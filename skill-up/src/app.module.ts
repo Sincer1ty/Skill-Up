a16d2a6204c9f5a14e4520966d0d21e6aba3763c
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-10-10 00:17:22 +0900

    deploy with ec2

--- skill-up/src/app.module.ts
+++ skill-up/src/app.module.ts
@@ -5,10 +5,15 @@ import { TestMiddleware, TestMiddleware2 } from './middleware/TestMiddleware';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BoardModule } from './board/board.module';
-import { UserModule } from './user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(),
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'static'),
+    serveRoot: '/'
+  }),
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
     BoardModule
   ],
