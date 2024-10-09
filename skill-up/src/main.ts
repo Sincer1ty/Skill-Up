9a6320f849ab7975b30b0aa654af95b7af80ce70
김성희 <87846251+Sincer1ty@users.noreply.github.com>
2024-10-09 17:06:17 +0900

    Update main.ts

--- skill-up/src/main.ts
+++ skill-up/src/main.ts
@@ -6,7 +6,7 @@ import { User } from './user/schemas/user.schema';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(
-    {origin: 'https://sincer1ty.github.io/', // 프론트엔드 도메인
+    {origin: 'https://sincer1ty.github.io', // 프론트엔드 도메인
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true}
   );
